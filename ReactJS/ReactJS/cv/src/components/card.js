842d47e508c659c2fcad9f69a32e0d49a9a81203
hoangnhatd <Nhatcycy@gmail.com>
2023-10-06 19:26:23 +0700

    first commit

---
+++ ReactJS/ReactJS/cv/src/components/card.js
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+export default function Card(props) {
+  return (
+    <>
+      <div className="col location">
+        <div className="card">
+          <img
+            src={props.data.pic}
+            className="card-img-top imgcard "
+            alt="Skyscrapers"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Product</h5>
+            <p className="card-text">{props.data.name}</p>
+            <p className="card-text">{props.data.price}</p>
+            {/* <button className="btn btn-primary" >Buy</button> */}
+            <NavLink to="/product-detail2" className="btn btn-primary">
+              Buy
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
